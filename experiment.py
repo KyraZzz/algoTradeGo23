2cd1aed0dcd6ed15af8e069a8b58cc146ec0c142
Mmmck5 <menchengkai@gmail.com>
2023-03-12 19:30:11 +0000

    experiment.py is based on market making strategy; experiment_2.py is based on stat_arb_full

--- experiment.py
+++ experiment.py
@@ -17,6 +17,10 @@
 #     <https://www.gnu.org/licenses/>.
 import asyncio
 import itertools
+# new
+import threading
+import numpy as np
+# end new
 
 from typing import List
 
@@ -28,6 +32,10 @@ POSITION_LIMIT = 100
 TICK_SIZE_IN_CENTS = 100
 ACTIVE_VOLUME_LIMIT = 200
 ACTIVE_ORDERS_LIMIT = 10
+# new
+INTERVAL = 60 # the optimisation interval in seconds, every second approx. 4 messages are received
+ZLB = 4e-4 # the zero lower bound for no profit
+# end new
 
 MIN_BID_NEAREST_TICK = (
     MINIMUM_BID + TICK_SIZE_IN_CENTS) // TICK_SIZE_IN_CENTS * TICK_SIZE_IN_CENTS
@@ -55,6 +63,36 @@ class AutoTrader(BaseAutoTrader):
         self.top_bid_dic = dict()
         self.top_ask_dic = dict()
         self.active_order_dict = dict()
+
+        # new
+        self.spreadA = []
+        self.spreadB = []
+        self.t_0 = 0 # time in units of sequence number : int
+        self.NewThreshold = 2e-3
+        self.Threshold = THRESHOLD # I replaced all THRESHOLD 
+        # end new
+
+    # new
+    def optimise(self,spreadA,spreadB):
+        spreadA = np.array(spreadA)
+        spreadB = np.array(spreadB)
+        spreadA = np.sort(spreadA[spreadA>ZLB])
+        spreadB = np.sort(spreadB[spreadB>ZLB])
+        bound1 = bound2 = self.Threshold # previous threshold
+
+        lenA = len(spreadA)
+        lenB = len(spreadB)
+
+        top70A = int(lenA*0.3)
+        top70B = int(lenB*0.3)
+        if lenA > 5:
+            bound1 = spreadA[top70A]
+        if lenB > 5:
+            bound2 = spreadB[top70B]
+
+        self.NewThreshold = np.mean([bound1,bound2])
+
+    # end new
 
     def on_error_message(self, client_order_id: int, error_message: bytes) -> None:
         """Called when the exchange detects an error.
@@ -103,6 +141,11 @@ class AutoTrader(BaseAutoTrader):
 
         self.logger.info("received order book for instrument %d with sequence number %d", instrument,
                          sequence_number)
+        
+        # new
+        current_time = sequence_number # current time in units of sequence number
+        # end new
+
         if instrument == Instrument.ETF:
             other = Instrument.FUTURE
             self.top_bid_dic[instrument] = [
@@ -115,10 +158,20 @@ class AutoTrader(BaseAutoTrader):
             e_ask_p0 = self.top_ask_dic[instrument][0][0]
             e_bid_p0 = self.top_bid_dic[instrument][0][0]
 
+            # new: record two spreads in percentage
+            spreadA = f_bid_p0 - (e_bid_p0 + TICK_SIZE_IN_CENTS)
+            spreadA = spreadA / (e_bid_p0 + TICK_SIZE_IN_CENTS)
+            spreadB = (e_ask_p0 - TICK_SIZE_IN_CENTS) - f_ask_p0
+            spreadB = spreadB / (e_ask_p0 - TICK_SIZE_IN_CENTS)
+            self.spreadA.append(spreadA)
+            self.spreadB.append(spreadB)
+            # end new
+
+
             # entry signal
             if abs(self.position) < POSITION_LIMIT and self.active_volume < ACTIVE_VOLUME_LIMIT and self.active_orders < ACTIVE_ORDERS_LIMIT\
                     and other in self.top_bid_dic.keys() and other in self.top_ask_dic.keys():
-                if f_bid_p0 - (e_bid_p0 + TICK_SIZE_IN_CENTS) >= THRESHOLD * (e_bid_p0 + TICK_SIZE_IN_CENTS):
+                if f_bid_p0 - (e_bid_p0 + TICK_SIZE_IN_CENTS) >= self.Threshold * (e_bid_p0 + TICK_SIZE_IN_CENTS):
                     # insert bid in etf, (if successful) hit bid in future
                     volume = min(MAX_LOT_SIZE, POSITION_LIMIT - self.position,
                                  ACTIVE_VOLUME_LIMIT - self.active_volume)
@@ -127,7 +180,7 @@ class AutoTrader(BaseAutoTrader):
                         self.send_insert_order(
                             self.bid_id, Side.BUY, (e_bid_p0 + TICK_SIZE_IN_CENTS), volume, Lifespan.G)
                         self.bids.add(self.bid_id)
-                if (e_ask_p0 - TICK_SIZE_IN_CENTS) - f_ask_p0 >= THRESHOLD * f_ask_p0:
+                if (e_ask_p0 - TICK_SIZE_IN_CENTS) - f_ask_p0 >= self.Threshold * f_ask_p0:
                     # insert ask in etf, (if successful) take offer in future
                     self.ask_id = next(self.order_ids)
                     volume = min(MAX_LOT_SIZE, POSITION_LIMIT + self.position,
@@ -138,11 +191,11 @@ class AutoTrader(BaseAutoTrader):
                         self.asks.add(self.ask_id)
 
             # cancel orders signal - when the spread is less than the THRESHOLD on either side
-            if f_bid_p0 - (e_bid_p0 + TICK_SIZE_IN_CENTS) < THRESHOLD * (e_bid_p0 + TICK_SIZE_IN_CENTS):
+            if f_bid_p0 - (e_bid_p0 + TICK_SIZE_IN_CENTS) < self.Threshold * (e_bid_p0 + TICK_SIZE_IN_CENTS):
                 for bid in self.bids:
                     self.send_cancel_order(bid)
                 self.bids = set()
-            if (e_ask_p0 - TICK_SIZE_IN_CENTS) - f_ask_p0 < THRESHOLD * f_ask_p0:
+            if (e_ask_p0 - TICK_SIZE_IN_CENTS) - f_ask_p0 < self.Threshold * f_ask_p0:
                 for ask in self.asks:
                     self.send_cancel_order(ask)
                 self.asks = set()
@@ -162,11 +215,28 @@ class AutoTrader(BaseAutoTrader):
                     self.bid_id, Side.BUY, e_ask_p0, volume, Lifespan.F)
                 self.bids.add(self.bid_id)
 
+            # new: threading the optimisation process
+            time_elapsed = current_time - self.t_0
+            if time_elapsed % 60 == 0:
+                print(self.Threshold)
+                spreadA = self.spreadA[-60:]
+                spreadB = self.spreadB[-60:]
+                x = threading.Thread(target=self.optimise,kwargs={'spreadA':spreadA,'spreadB':spreadB})
+                x.start()
+
+                if time_elapsed > 60:
+                    self.Threshold = self.NewThreshold
+                    print(self.Threshold)
+
+            # end new
+
+
         elif instrument == Instrument.FUTURE:
             self.top_bid_dic[instrument] = [
                 (price, bid_volumes[idx]) for idx, price in enumerate(bid_prices)]
             self.top_ask_dic[instrument] = [
                 (price, ask_volumes[idx]) for idx, price in enumerate(ask_prices)]
+
 
     def on_order_filled_message(self, client_order_id: int, price: int, volume: int) -> None:
         """Called when one of your orders is filled, partially or fully.
